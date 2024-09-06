46e11aea0c2608d5379079914efe0fb16e8f3789
Si-rauis <kkyd7189@gmail.com>
2024-09-06 10:36:44 +0900

    소스 코드

---
+++ gc-coffee/src/test/java/edu/example/gccoffee/GcCoffeeApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.example.gccoffee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcCoffeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
