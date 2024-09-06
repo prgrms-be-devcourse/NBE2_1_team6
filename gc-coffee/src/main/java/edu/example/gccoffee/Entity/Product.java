3e8a33b5a2eb3f589a449ed5feec33495c0e70fe
남현호 <143480257+namhyeonh0@users.noreply.github.com>
2024-09-06 14:47:35 +0900

    commit Product.java

--- gc-coffee/src/main/java/edu/example/gccoffee/Entity/Product.java
+++ gc-coffee/src/main/java/edu/example/gccoffee/Entity/Product.java
@@ -1,4 +1,45 @@
 package edu.example.gccoffee.Entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "products")
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long productId;
+
+    @Column(nullable = false)
+    private String productName;
+
+    @Column(nullable = false)
+    private String category;
+
+    @Column(nullable = false)
+    private int price;
+
+    private String description;
+
+    public void changeProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public void changeCategory(Category category) {
+        this.category = category;
+    }
+
+    public void changePrice(int price) {
+        this.price = price;
+    }
+
+    public void changeDescription(String description) {
+        this.description = description;
+    }
 }
