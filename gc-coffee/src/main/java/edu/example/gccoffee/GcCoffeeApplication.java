da6281a8611dd0f7b1bfa2acf79538344df310dc
Si-rauis <kkyd7189@gmail.com>
2024-09-06 14:43:39 +0900

    Entity Create

--- gc-coffee/src/main/java/edu/example/gccoffee/GcCoffeeApplication.java
+++ gc-coffee/src/main/java/edu/example/gccoffee/GcCoffeeApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GcCoffeeApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(GcCoffeeApplication.class, args);
     }
