dd41c25e7662385cbf049a39f5bc11cd5413c40a
newbie_all_the_time <tupac52@gmail.com>
2024-09-07 17:35:56 +0900

    Update Test, Read Test 추가

--- gc-coffee/src/test/java/edu/example/gccoffee/repository/ProductRepositoryTests.java
+++ gc-coffee/src/test/java/edu/example/gccoffee/repository/ProductRepositoryTests.java
@@ -3,6 +3,7 @@ package edu.example.gccoffee.repository;
 import edu.example.gccoffee.entity.Category;
 import edu.example.gccoffee.entity.Product;
 import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -10,6 +11,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -30,7 +34,7 @@ public class ProductRepositoryTests {
 
         //THEN
         assertNotNull(savedProduct);
-        assertEquals(1, savedProduct.getProductId());
+        assertEquals(4, savedProduct.getProductId());
         assertEquals("상품1", savedProduct.getProductName());
         assertEquals("COFFEE_BEAN_PACKAGE", savedProduct.getCategory());
         assertEquals(3000, savedProduct.getPrice());
@@ -38,9 +42,45 @@ public class ProductRepositoryTests {
     }
 
     @Test
+    @DisplayName("업데이트 테스트")
+    public void testUpdate() {
+        Long productId = 2L;
+
+        Optional<Product> foundProduct = productRepository.findById(productId);
+        assertTrue(foundProduct.isPresent(), "Product should be present");
+
+        Product product = foundProduct.get();
+        product.changeProductName("제품명 재업데이트");
+        product.changePrice(100000);
+        product.changeDescription("제품 설명 재업데이트");
+
+        productRepository.save(product);
+
+        assertThat(product.getProductName()).isEqualTo("제품명 재업데이트");
+        assertThat(product.getPrice()).isEqualTo(100000);
+        assertThat(product.getDescription()).isEqualTo("제품 설명 재업데이트");
+    }
+
+    @Test
+    @DisplayName("읽어오기 테스트")
+    public void testRead() {
+        Long productId = 2L;
+
+        Optional<Product> foundProduct = productRepository.findById(productId);
+        assertTrue(foundProduct.isPresent(), "Product should be present");
+
+        Product product = foundProduct.get();
+        assertNotNull(product);
+        assertEquals(2, product.getProductId());
+        assertEquals(100000, product.getPrice());
+        assertEquals("제품명 재업데이트", product.getProductName());
+        assertEquals("제품 설명 재업데이트", product.getDescription());
+    }
+
+    @Test
     public void testDelete() {
         //GIVEN
-        Long productId = 1L;
+        Long productId = 2L;
 
         //WHEN
         assertTrue(productRepository.existsById(productId));
