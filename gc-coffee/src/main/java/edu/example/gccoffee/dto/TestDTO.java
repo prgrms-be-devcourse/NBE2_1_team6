1e8ff5f88e8475c097656e1a1acffbe2986aaea4
남현호 <143480257+namhyeonh0@users.noreply.github.com>
2024-09-09 14:53:28 +0900

    TestDTO.java 수정

--- gc-coffee/src/main/java/edu/example/gccoffee/dto/TestDTO.java
+++ gc-coffee/src/main/java/edu/example/gccoffee/dto/TestDTO.java
@@ -5,4 +5,17 @@ public class TestDTO {
     public void test() {
         System.out.println("테스트");
     }
+
+    public void test1() {
+        System.out.println("테스트");
+    }
+
+    public void test2() {
+        System.out.println("테스트");
+    }
+
+    public void test3() {
+        System.out.println("테스트");
+    }
+
 }
