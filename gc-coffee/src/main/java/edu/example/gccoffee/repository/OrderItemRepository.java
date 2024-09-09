0cf5072840fce45b163f7e20a2f83039a8fbc243
Si-rauis <kkyd7189@gmail.com>
2024-09-09 11:11:15 +0900

    오류 수정

--- gc-coffee/src/main/java/edu/example/gccoffee/repository/OrderItemRepository.java
+++ gc-coffee/src/main/java/edu/example/gccoffee/repository/OrderItemRepository.java
@@ -1,7 +1,6 @@
 package edu.example.gccoffee.repository;
 
 
-import edu.example.gccoffee.entity.Order;
 import edu.example.gccoffee.entity.OrderItem;
 import edu.example.gccoffee.repository.search.OrderItemSearch;
 import org.springframework.data.jpa.repository.JpaRepository;
