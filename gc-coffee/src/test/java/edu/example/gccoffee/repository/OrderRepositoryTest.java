74f9d49f4638a81b82cff6685f5553505cf9f18b
Si-rauis <kkyd7189@gmail.com>
2024-09-09 10:39:15 +0900

    Order 어노테이션 추가

--- gc-coffee/src/test/java/edu/example/gccoffee/repository/OrderRepositoryTest.java
+++ gc-coffee/src/test/java/edu/example/gccoffee/repository/OrderRepositoryTest.java
@@ -3,7 +3,9 @@ package edu.example.gccoffee.repository;
 import edu.example.gccoffee.entity.Order;
 import edu.example.gccoffee.entity.OrderStatus;
 import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -20,11 +22,13 @@ import static org.junit.jupiter.api.Assertions.*;
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @Transactional(propagation = Propagation.NOT_SUPPORTED)
 @Log4j2
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class OrderRepositoryTest {
     @Autowired
     private OrderRepository orderRepository;
 
     @Test
+    @org.junit.jupiter.api.Order(1)
     public void testInsert() {
         IntStream.rangeClosed(1, 5).forEach(i -> {
             Order order = Order.builder()
@@ -44,17 +48,19 @@ public class OrderRepositoryTest {
     }
 
     @Test
+    @org.junit.jupiter.api.Order(2)
     public void testRead() {
         Long orderId = 1L;
 
         Optional<Order> order = orderRepository.findById(orderId);
 
-        log.info(order.get());
+        log.info(order.isPresent());
         assertNotNull(order);
         assertEquals(orderId, order.get().getOrderId());
     }
 
     @Test
+    @org.junit.jupiter.api.Order(3)
     public void testUpdate() {
         Long orderId = 1L;
         String address = "변경 주문";
