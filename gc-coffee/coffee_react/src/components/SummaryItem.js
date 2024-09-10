cf311601129d91805965ea506ce2c46df83bf5f0
newbie_all_the_time <tupac52@gmail.com>
2024-09-10 21:55:38 +0900

    react_ois_인덴트 조정, 결제하기 버튼 클릭 시 발생하는 에러 확인을 위한 로그 추가

--- gc-coffee/coffee_react/src/components/SummaryItem.js
+++ gc-coffee/coffee_react/src/components/SummaryItem.js
@@ -1,9 +1,9 @@
 import React from "react";
 
 export function SummaryItem({productName, count}) {
-  return (
-    <div className="row">
-      <h6 className="p-0">{productName} <span className="badge bg-dark text-">{count}개</span></h6>
-    </div>
-  )
+    return (
+        <div className="row">
+            <h6 className="p-0">{productName} <span className="badge bg-dark text-">{count}개</span></h6>
+        </div>
+    )
 }
