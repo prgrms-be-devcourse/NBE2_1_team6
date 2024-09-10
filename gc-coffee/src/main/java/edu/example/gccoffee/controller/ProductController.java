549bf1416391cdd69973415046fb4a432ad756ef
남현호 <143480257+namhyeonh0@users.noreply.github.com>
2024-09-10 15:26:49 +0900

    controller & view 작업

--- gc-coffee/src/main/java/edu/example/gccoffee/controller/ProductController.java
+++ gc-coffee/src/main/java/edu/example/gccoffee/controller/ProductController.java
@@ -1,49 +1,34 @@
 package edu.example.gccoffee.controller;
 
-import edu.example.gccoffee.dto.PageRequestDTO;
 import edu.example.gccoffee.dto.ProductDTO;
 import edu.example.gccoffee.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.data.domain.Page;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
-import java.util.List;
-import java.util.Map;
-
-@RestController
-@RequestMapping("/api/v1/products")
+@Controller
 @RequiredArgsConstructor
 @Log4j2
 public class ProductController {
     private final ProductService productService;
 
-    @GetMapping("/lists")
-    public ResponseEntity<List<ProductDTO>> getList() {
-        return ResponseEntity.ok(productService.getList());
+    @GetMapping("/products")
+    public String productsPage(Model model) {
+        model.addAttribute("products", productService.getList());
+        return "product/product-list";
     }
 
-    @GetMapping("/pages")
-    public ResponseEntity<Page<ProductDTO>> getPage(@Validated PageRequestDTO pageRequestDTO) {
-        return ResponseEntity.ok(productService.getPage(pageRequestDTO));
+    @GetMapping("new-product")
+    public String newProductPage() {
+        return "product/new-product";
     }
 
-    @PostMapping
-    public ResponseEntity<ProductDTO> create(@Validated @RequestBody ProductDTO productDTO) {
-        return ResponseEntity.ok(productService.create(productDTO));
-    }
-
-    @PutMapping("/{productId}")
-    public ResponseEntity<ProductDTO> update(@Validated @RequestBody ProductDTO productDTO, @PathVariable Long productId) {
-        productDTO.setProductId(productId);
-        return ResponseEntity.ok(productService.update(productDTO));
-    }
-
-    @DeleteMapping("/{productId}")
-    public ResponseEntity<Map<String, String>> delete(@PathVariable Long productId) {
-        productService.delete(productId);
-        return ResponseEntity.ok(Map.of("result", "success"));
+    @PostMapping("/products")
+    public String newProduct(ProductDTO productDTO) {
+        productService.create(productDTO);
+        return "redirect:/products";
     }
 }
