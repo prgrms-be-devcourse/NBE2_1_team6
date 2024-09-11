c85746a321bbb33d436dc04750e53c0224fb8824
Si-rauis <kkyd7189@gmail.com>
2024-09-11 11:08:40 +0900

    서버포트 8080 ProductRestController Get 추가

--- gc-coffee/src/main/java/edu/example/gccoffee/controller/ProductRestController.java
+++ gc-coffee/src/main/java/edu/example/gccoffee/controller/ProductRestController.java
@@ -8,9 +8,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,13 +19,13 @@ import java.util.List;
 public class ProductRestController {
     private final ProductService productService;
 
-    @GetMapping
-    public ResponseEntity<List<ProductDTO>> getList() {
-        return ResponseEntity.ok(productService.getList());
-    }
-
     @GetMapping("/pages")
     public ResponseEntity<Page<ProductDTO>> getPage(@Validated PageRequestDTO pageRequestDTO) {
         return ResponseEntity.ok(productService.getPage(pageRequestDTO));
+    }
+
+    @GetMapping("/lists")
+    public ResponseEntity<List<ProductDTO>> getList() {
+        return ResponseEntity.ok(productService.getList());
     }
 }
