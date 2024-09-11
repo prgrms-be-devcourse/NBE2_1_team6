82f30dbe09985e60465449a8a460ff2ac1ef7bcc
남현호 <143480257+namhyeonh0@users.noreply.github.com>
2024-09-11 10:17:11 +0900

    view 수정&삭제 작업 완료

--- gc-coffee/src/main/java/edu/example/gccoffee/controller/ProductController.java
+++ gc-coffee/src/main/java/edu/example/gccoffee/controller/ProductController.java
@@ -7,9 +7,9 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
@@ -23,25 +23,32 @@ public class ProductController {
         return "product/product-list";
     }
 
-    @GetMapping("new-product")
+    @GetMapping("product/create")
     public String newProductPage() {
         return "product/new-product";
     }
 
-    @GetMapping("modify-product")
-    public String modifyProductPage(@RequestParam Long id, Model model) {
-        model.addAttribute(productService.read(id));
-        return "product/modify-product";
-    }
-
     @PostMapping("/products")
-    public String newProduct(ProductDTO productDTO) {
+    public String createProduct(ProductDTO productDTO) {
         productService.create(productDTO);
         return "redirect:/products";
     }
 
-    @GetMapping("/test")
-    public String test() {
-        return "product/test";
+    @GetMapping("product/update/{id}")
+    public String modifyProductPage(@PathVariable Long id, Model model) {
+        model.addAttribute("product", productService.read(id));
+        return "product/modify-product";
+    }
+
+    @PostMapping("products/update")
+    public String updateProduct(@ModelAttribute ProductDTO productDTO) {
+        productService.update(productDTO);
+        return "redirect:/products";
+    }
+
+    @GetMapping("product/delete/{id}")
+    public String deleteProduct(@PathVariable Long id) {
+        productService.delete(id);
+        return "redirect:/products";
     }
 }
